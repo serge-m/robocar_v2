fe400589d425060ce224c1d6da7b0b05c9e3e5df
SergeM <sbmatyunin@gmail.com>
2020-02-23 22:45:48 +0100

    publish dummy direction vector

--- catkin_ws/src/lane_follow/src/lf/lane_follower.py
+++ catkin_ws/src/lane_follow/src/lf/lane_follower.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
 import numpy as np
+from typing import Dict, Optional
 
 class LaneFollower:
     def __init__(self):
         pass
     
-    def __call__(self, image: np.ndarray) -> np.ndarray:
-        image = image.copy()
-        image[:image.shape[0] // 2] = 0
-        return image
+    def __call__(self, image: np.ndarray, debug_output: Optional[Dict[str, object]]=None) -> np.ndarray:
+        if debug_output is not None:
+            vis = image.copy()
+            vis[:image.shape[0] // 2] = vis[:image.shape[0] // 2] // 2
+            debug_output['vis'] = vis
+        return image.ravel()[:3]
--- catkin_ws/src/lane_follow/src/lf_node.py
+++ catkin_ws/src/lane_follow/src/lf_node.py
@@ -10,33 +10,36 @@ import cv2
 import roslib
 import rospy
 
-from sensor_msgs.msg import CompressedImage
-from lf.lane_follower import LaneFollower
 
+from geometry_msgs.msg import Vector3Stamped, Vector3
+from sensor_msgs.msg import CompressedImage
+
+from lf.lane_follower import LaneFollower
+from img_messages import compressed_img_message, np_from_compressed_ros_msg
+
+
+def vector3d_msg(vector: np.ndarray) -> Vector3Stamped:
+    msg = Vector3Stamped()
+    msg.header.stamp=rospy.Time.now()
+    msg.vector=Vector3(vector[0], vector[1], vector[2])
+    return msg
 
 class ImageSubPubNode:
     def __init__(self, processor: callable):
         self.subscriber = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, self.callback,  queue_size=1)
-        self.publisher = rospy.Publisher("/lane_follow/debug/compressed", CompressedImage, queue_size=1)
+        self.publisher_debug = rospy.Publisher("/lane_follow/debug/compressed", CompressedImage, queue_size=1)
+        self.publisher_shift = rospy.Publisher("/lane_follow/desired_shift", Vector3Stamped, queue_size=1)
         rospy.on_shutdown(self.shutdown)
         self.processor = processor
     
     def callback(self, ros_data):
-        np_arr = np.fromstring(ros_data.data, np.uint8)
-        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-        result = self.processor(image_np)
-        msg = self.compressed_img_message(result)
-        self.publisher.publish(msg)
-
-    def compressed_img_message(self, image_np):
-        msg = CompressedImage()
-        msg.header.stamp = rospy.Time.now()
-        msg.format = "jpeg"
-        retval, encoded = cv2.imencode('.jpg', image_np)
-        if not retval:
-            raise RuntimeError("Unable to encode image, {}".format(image_np))
-        msg.data = np.array(encoded).tostring()
-        return msg
+        image_np = np_from_compressed_ros_msg(ros_data)
+        debug_output = {}
+        desired_shift = self.processor(image_np, debug_output)
+        self.publisher_shift.publish(vector3d_msg(desired_shift))
+        if 'vis' in debug_output:
+            self.publisher_debug.publish(compressed_img_message(debug_output['vis']))
+        
 
     def shutdown(self):
         print("ImageSubPubNode shut down")
