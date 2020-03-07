898166eaa43cb678a51ca3bdb9785fa028f055ab
SergeM <sbmatyunin@gmail.com>
2020-03-07 13:20:22 +0100

    multithreading driver publisher

--- catkin_ws/src/driver/src/driver.py
+++ catkin_ws/src/driver/src/driver.py
@@ -12,23 +12,36 @@ import rospy
 
 from ackermann_msgs.msg import AckermannDrive
 from pwm_radio_arduino.msg import steering_tfm
+import threading
+
+
+def start_thread_pub_tfm_params():
+    thread = threading.Thread(target=pub_tfm_params,args=())
+    thread.setDaemon(True)
+    thread.start()
+
+def pub_tfm_params():
+    pub_tfm = rospy.Publisher("pwm_radio_arduino/steering_tfm", steering_tfm, queue_size=1, latch=False)
+    while not rospy.is_shutdown():
+        tfm = steering_tfm(-1, 0, 1, 1200, 1400, 1600, -1, 0, 1, 1300, 1400, 1500)
+        pub_tfm.publish(tfm)  
+        rospy.loginfo("pub_tfm")
+        time.sleep(1) 
 
 def talker():
     pub = rospy.Publisher("pwm_radio_arduino/driver_ackermann", AckermannDrive, queue_size=1, latch=False)
-    pub_tfm = rospy.Publisher("pwm_radio_arduino/steering_tfm", steering_tfm, queue_size=1, latch=False)
-    rospy.init_node('driver', anonymous=True)
     rate = rospy.Rate(5) # 10hz
     while not rospy.is_shutdown():
         for angle in np.hstack([np.arange(-1, 1, 0.1), np.arange(1, -1, -0.1)]):
             msg = AckermannDrive(speed=0, steering_angle=angle)
             pub.publish(msg)
-            tfm = steering_tfm(-1, 0, 1, 1200, 1400, 1600, -1, 0, 1, 1300, 1400, 1500)
-            pub_tfm.publish(tfm)    
-            print("1")    
+            rospy.loginfo("pub_drive")
             rate.sleep()
 
 if __name__ == '__main__':
     try:
+        rospy.init_node('driver', anonymous=True)
+        start_thread_pub_tfm_params()
         talker()
     except rospy.ROSInterruptException:
         pass
