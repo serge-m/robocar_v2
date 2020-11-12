3848c6a13899e8c4697ded3084c1998f9fb4916e
CatUnderTheLeaf <stacy.chaikovska@gmail.com>
2020-11-12 12:27:50 +0100

    lane follow test: - added images to test folder - changed path to test images

--- catkin_ws/src/lane_follow/src/debug_lane_follow.py
+++ catkin_ws/src/lane_follow/src/debug_lane_follow.py
@@ -1,4 +1,5 @@
 import sys, time
+import os
 import cv2
 import math
 # from lane_follower import LaneFollower
@@ -51,29 +52,31 @@ def test_lane_follow(image):
     return lane_follow.birdsEyeImage.birds_image
 
 def main(args):
-    image_np = cv2.imread('C:/tmp/img/1.jpg')    
+    test_dir_path = os.path.join(os.path.split(os.path.dirname(__file__))[0], 'test')
+    fname = os.path.join(test_dir_path, '1.jpg')
+    image_np = cv2.imread(fname)   
     # testing binary tresholding
     treshold_image = test_treshold(image_np)
-    # cv2.imwrite('C:/tmp/img/treshold_image.jpg', treshold_image)
+    # cv2.imwrite(os.path.join(test_dir_path, 'treshold_image.jpg'), treshold_image)
     
     # testing warp functions
     birds_image = test_birdsEye(treshold_image)
-    # cv2.imwrite('C:/tmp/img/birds_image.jpg', birds_image)
+    # cv2.imwrite(os.path.join(test_dir_path, 'birds_image.jpg'), birds_image)
     
     # testing fitting polynomial
     left_fit, right_fit, poly_img = test_fit_polynomial(birds_image)
     print("left fit", str(left_fit))
     print("right fit", str(right_fit))
-    cv2.imwrite('C:/tmp/img/poly.jpg', poly_img)
+    # cv2.imwrite(os.path.join(test_dir_path, 'poly.jpg'), poly_img)
 
     # testing getting new waypoints
     waypoints = test_get_waypoints(birds_image)
     print("waypoints", waypoints)
 
     # testing whole laneFollower
-    # image_test = cv2.imread('C:/tmp/img/1.jpg')
+    # image_test = cv2.imread(fname)
     # lf_img = test_lane_follow(image_test)
-    # cv2.imwrite('C:/tmp/img/lf_img.jpg', lf_img)
+    # cv2.imwrite(os.path.join(test_dir_path, 'lf_img.jpg'), lf_img)
     
     
 if __name__ == '__main__':
