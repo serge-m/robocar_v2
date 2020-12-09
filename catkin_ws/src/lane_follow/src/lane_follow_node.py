086faf818cc08d377db54b37441ca2866e50b522
CatUnderTheLeaf <stacy.chaikovska@gmail.com>
2020-12-09 14:12:22 +0100

    added params

--- catkin_ws/src/lane_follow/src/lane_follow_node.py
+++ catkin_ws/src/lane_follow/src/lane_follow_node.py
@@ -30,20 +30,17 @@ class LaneFollowNode:
         self.canTransformImg = False
 
         # load sensor_msgs/CameraInfo Message from yaml file if it exists
-        # file_camera_params = rospy.get_param('~camera_params')
-        file_camera_params = "package://robocar_simulation/config/sim_camera_calibration.yaml"
-        self.cam_info_mng = camera_info_manager.CameraInfoManager(cname='camera', url=file_camera_params)
-        self.cam_info_mng.loadCameraInfo()
-        # if there is no yaml file msg is equal to null, no error msg
-        camera_info_msg = self.cam_info_mng.getCameraInfo()              
-        if (camera_info_msg):  
-            self.camera_model.fromCameraInfo(camera_info_msg)
-                
+        file_camera_params = rospy.get_param('~camera_params')
+        camera_info_msg = camera_info_manager.loadCalibrationFile(file_camera_params, cname='camera')
+        # if there is no yaml file msg is filled with zeros, no error msg
+        if (camera_info_msg.width != 0):              
+            self.info_cb(camera_info_msg)
+        else:
+            # get all parameters from sensor_msgs/CameraInfo Message
+            self.camera_info_sub = rospy.Subscriber("/camera/color/camera_info", CameraInfo, self.info_cb,  queue_size=1)
+                        
         # get camera position (in metres, degrees) from tf topic
-        self.tf_listener = tf.TransformListener()
-        
-        # get all parameters from sensor_msgs/CameraInfo Message
-        self.camera_info_sub = rospy.Subscriber("/camera/color/camera_info", CameraInfo, self.info_cb,  queue_size=1)
+        self.tf_listener = tf.TransformListener()       
         
         # image after rectification, no need to undistort
         self.img_sub = rospy.Subscriber("/camera/color/image_rect_color", Image, self.img_cb,  queue_size=1)
@@ -55,30 +52,31 @@ class LaneFollowNode:
         rate = rospy.Rate(30)
         
         while not rospy.is_shutdown(): 
-            # TODO test for null values 
-            # if it is true - camera model is loaded
+            # camera model is loaded but TransformMatrix is not ready
             if self.cameraInfoSet and (not self.hasTransformMatrix):
                 try:
                     # find src and dst points for 
                     # LaneFollower.image_proc.get_transform_matrix(src, dst)
                     # it is done here because points 
                     # should be transformed between frames with tfTransformer
-                    h = self.cam_info_mng.getCameraInfo().height
-                    w = self.cam_info_mng.getCameraInfo().width
+                    h = self.camera_model.height
+                    w = self.camera_model.width
                     # left bottom corner        
                     lbc_ray = self.camera_model.projectPixelTo3dRay((0, h))
                     # right bottom corner        
                     rbc_ray = self.camera_model.projectPixelTo3dRay((w, h))
                     # camera center and bottom points in the base_bottom_link frame
-                    zero = self.get_point_xyz(self.transformPoint((0,0,0), "camera_link_optical", "base_bottom_link"))
-                    lbc_point = self.get_point_xyz(self.transformPoint(lbc_ray, "camera_link_optical", "base_bottom_link"))
-                    rbc_point = self.get_point_xyz(self.transformPoint(rbc_ray, "camera_link_optical", "base_bottom_link"))
+                    camera_link_optical = rospy.get_param('~camera_opt_frame')
+                    base_bottom_link = rospy.get_param('~base_frame')
+                    zero = self.get_point_xyz(self.transformPoint((0,0,0), camera_link_optical, base_bottom_link))
+                    lbc_point = self.get_point_xyz(self.transformPoint(lbc_ray, camera_link_optical, base_bottom_link))
+                    rbc_point = self.get_point_xyz(self.transformPoint(rbc_ray, camera_link_optical, base_bottom_link))
                     # get scale parameter from params server
                     y_scale = rospy.get_param('~y_scale')
                     point3, point4, x_scale = getUpperPoints(zero, lbc_point, rbc_point, y_scale)
                     # transform points 3 and 4 to camera_optical_link frame
-                    luc_point = self.get_point_xyz(self.transformPoint(point3, "base_bottom_link", "camera_link_optical"))
-                    ruc_point = self.get_point_xyz(self.transformPoint(point4, "base_bottom_link", "camera_link_optical"))
+                    luc_point = self.get_point_xyz(self.transformPoint(point3, base_bottom_link, camera_link_optical))
+                    ruc_point = self.get_point_xyz(self.transformPoint(point4, base_bottom_link, camera_link_optical))
                     # pixel coordinates of these points:
                     # left upper corner 
                     luc = self.camera_model.project3dToPixel(luc_point)
@@ -101,7 +99,6 @@ class LaneFollowNode:
                     top_view_msg = self.bridge.cv2_to_imgmsg(self.lane_follower.image_proc.birds_image)
                     self.img_pub.publish(top_view_msg)
                     # publish Lane
-                    # self.tf_listener.waitForTransform("world", "base_link", rospy.Time.now(), rospy.Duration(4.0))
                     self.publisher.publish(self.makeLane(self.lane_follower.waypoints))
                     rate.sleep()
                 except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
@@ -110,11 +107,11 @@ class LaneFollowNode:
     # make a robocar_msgs.Lane from waypoints
     def makeLane(self, waypoints):
         msg = []
-        
+        base_bottom_link = rospy.get_param('~base_frame')
         for i in range(len(waypoints)):
             new_point = Waypoint()
-            # apply transformation to a pose between 'base_bottom_link' and 'world' frames
-            new_point.pose = self.transformPoint(waypoints[i], "base_bottom_link", "world")
+            # apply transformation to a pose between 'base_bottom_link' and 'world' frames            
+            new_point.pose = self.transformPoint(waypoints[i], base_bottom_link, "world")
             new_point.twist.twist.linear.x = float(self.velocity)
             msg.append(new_point) 
 
@@ -148,11 +145,7 @@ class LaneFollowNode:
     def info_cb(self, msg):
         if not self.cameraInfoSet:
             self.camera_model.fromCameraInfo(msg)
-            self.cameraInfoSet = True
-            # TODO test if there is no calibration file
-            if not self.cam_info_mng.getCameraInfo():
-                self.cam_info_mng.saveCalibration(msg, file_camera_params, 'camera')
-                self.cam_info_mng.loadCameraInfo()
+            self.cameraInfoSet = True    
             
     def img_cb(self, data):
         if (self.hasTransformMatrix):
